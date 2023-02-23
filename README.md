4af13bc06c74e75c73d3f84c7d67ad4ec4065e1f
zhili.zheng <zhilizheng@outlook.com>
2023-02-23 01:54:01 -0500

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+
+Very simple code to extract pQTL results, usage
+ ```
+ bash extract-pQTL.sh {somascan, olink, decode} probs_{somascan, olink, decode}.txt $YOUR_REGEION $outputDir
+ ```
+
+YOUR\_REGION is a text format with each line the region interested "CHR POSstart POSend"
+
+outputDir will be created automatically. The files are headers.txt (headers for all summary), ${probe\_name}.txt (summary of the probe)
+
+The probs_\*.txt could be divided into smaller files for parallel.  
+
+
+
---
+++ extract-pQTL.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: extractProteomics.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Zhili
+#  ORGANIZATION: 
+#       CREATED: 02/14/23 03:39:12
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+
+platform=$1
+probList=$2
+
+region=$3
+
+outDir=$4
+
+mkdir -p "$outDir"
+
+inputSource=""
+if [ $platform = "somascan" ]; then
+    inputSource="gs://zz-red/Omics/Proteomics/Soma2/pQTL-compress/SomaScan_Batch2"
+elif [ $platform = "olink" ]; then
+    inputSource="gs://zz-red/Omics/Proteomics/Olink/pQTL-compress/Olink_Batch1"
+elif [ $platform = "decode" ]; then
+    inputSource="gs://zz-red/Omics/Proteomics/deCODE/pQTL/deCODE"
+else
+    echo "the first parameter could only be: somasan or olink"
+    exit 1
+fi
+
+export GCS_OAUTH_TOKEN=`gcloud auth print-access-token`
+readarray -t traits < $probList
+
+gsutil cat ${inputSource}_${traits[0]}.txt.gz | zcat | head -n 1 > $outDir/headers.txt
+
+echo "Start to extract ${#traits[@]} probes"
+template=$region
+i=0
+for trait in "${traits[@]}"; do
+    ((i=i+1))
+    if [ ! -f "$outDir/$trait.txt" ] || [ ! -s "$outDir/$trait.txt" ]; then
+        echo "$i: $trait"
+        tabix -R $template ${inputSource}_${trait}.txt.gz > $outDir/$trait.txt
+    else
+        echo "$i: $trait exists"
+    fi
+done
+
